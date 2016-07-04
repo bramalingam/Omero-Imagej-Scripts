88d384ba4a3d28477f07214ac71996ec185a52ee
Balaji Ramalingam <b.ramalingam@dundee.ac.uk>
2016-07-04 15:20:57 +0100

    cleanup and add uploadImage

--- omero_batch_analysis.py
+++ omero_batch_analysis.py
@@ -6,6 +6,8 @@ from java.lang import String
 from java.lang.Long import longValue
 from java.util import ArrayList
 from jarray import array
+from java.lang.reflect import Array
+import java
 
 # Omero Dependencies
 from omero.gateway import Gateway
@@ -22,28 +24,16 @@ from omero.log import Logger
 from omero.log import SimpleLogger
 from omero.model import Pixels
 
-# Setup
-# =====
-
-# Drop omero_client.jar and Blitz.jar under the jars folder of FIJI
-
-# Parameters
-# ==========
-
-# open Omero Image
-# ================
-
-HOST = "omero-balaji.docker.openmicroscopy.org"
-USERNAME = "member-all-1"
-PASSWORD = "ome"
-PORT = 4064
-
-datasetId = "2"
-imageId = "1"
-groupId = "-1"
-
-
-
+from ome.formats.importer import ImportConfig
+from ome.formats.importer import OMEROWrapper
+from ome.formats.importer import ImportLibrary
+from ome.formats.importer import ImportCandidates
+from ome.formats.importer.cli import ErrorHandler
+from ome.formats.importer.cli import LoggingImportMonitor
+import loci.common
+from loci.formats.in import DefaultMetadataOptions
+from loci.formats.in import MetadataLevel
+from ij import IJ
 
 def openImagePlus(HOST,USERNAME,PASSWORD,groupId,imageId):
 	
@@ -60,9 +50,6 @@ def openImagePlus(HOST,USERNAME,PASSWORD,groupId,imageId):
 	options += imageId
 	options += "]"
 	options += " windowless=true "
-
-	print options
-	from ij import IJ
 
 	IJ.runPlugIn("loci.plugins.LociImporter", options);
 
@@ -97,20 +84,20 @@ def getImageIds(gateway, datasetId):
 	return imageIds
 
 
-def uploadImage(gateway):
+def uploadImage(path, gateway):
 
 	user = gateway.getLoggedInUser()
 	ctx = SecurityContext(user.getGroupId())
-	sessionKey = gateway.SessionId(user)
+	sessionKey = gateway.getSessionId(user)
 
-	config = Importconfig()
+	config = ImportConfig()
 
 	config.email.set("")
-	config.sendFiles.set(true)
-	config.sendReport.set(false)
-	config.contOnError.set(false)
-	config.debug.set(false)
-	config.hostname.set(HOSTNAME)
+	config.sendFiles.set('true')
+	config.sendReport.set('false')
+	config.contOnError.set('false')
+	config.debug.set('false')
+	config.hostname.set(HOST)
 	config.sessionKey.set(sessionKey)
 	config.targetClass.set("omero.model.Dataset")
 	config.targetId.set(datasetId)
@@ -118,20 +105,54 @@ def uploadImage(gateway):
 	loci.common.DebugTools.enableLogging("DEBUG")
 
 	store = config.createStore()
-#    store.logVersionInfo(config.getIniVersionNumber())
 	reader = OMEROWrapper(config)
 
 	library = ImportLibrary(store,reader)
 	errorHandler = ErrorHandler(config)
 
+	library.addObserver(LoggingImportMonitor())
+	candidates = ImportCandidates (reader, path, errorHandler)
+	reader.setMetadataOptions(DefaultMetadataOptions(MetadataLevel.ALL))
+	success = library.importCandidates(config, candidates)
+	return success
 	
+# Setup
+# =====
+
+# Drop omero_client.jar and Blitz.jar under the jars folder of FIJI
+
+# Parameters
+# ==========
+
+# open Omero Image
+# ================
+
+HOST = "tadpole.dyn.lifesci.dundee.ac.uk"
+USERNAME = "ome-trainer-1"
+PASSWORD = "ome"
+PORT = 4064
+
+datasetId = "399"
+imageId = "1"
+groupId = "-1"
+#paths = ArrayList()
+paths= "/Users/bramalingam/Desktop/Screen Shot 2016-06-22 at 09.48.57.png"
+str2d = java.lang.reflect.Array.newInstance(java.lang.String,[1])
+str2d [0] = paths
+
 # Prototype analysis example
 gateway = omeroConnect()
 imageIds = getImageIds(gateway,datasetId);
 imageIds.sort()
-imageId = imageIds[2]
-print imageId
-openImagePlus(HOST,USERNAME,PASSWORD,groupId,imageId)
+for imageId in imageIds:
+#	imageId = imageIds[2]
+	print imageId
+	openImagePlus(HOST,USERNAME,PASSWORD,groupId,imageId)
+	IJ.run("Enhance Contrast", "saturated=0.35");
+	#Plug Your analysis here#
+
+#success = uploadImage(str2d, gateway)
 gateway.disconnect()	
+
 	
 
